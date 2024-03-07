5b75650b9ba58352eafca1369dbfc47ed8c55d7d
Aron Fannarsson <aron.orri@gmail.com>
2024-03-07 13:57:01 -0500

    Initial commit, tiles are rendering

--- Assets/Naive/Scripts/TileManager.cs
+++ Assets/Naive/Scripts/TileManager.cs
@@ -1,12 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TileManager : MonoBehaviour
 {
-    private readonly uint WIDTH=64, HEIGHT=64;
+    private readonly uint WIDTH=128, HEIGHT=128;
     [SerializeField] private GameObject tilePrefab;
-    private GameObject[,] tiles;
+    private SpriteRenderer[,] tiles;
+    private bool[,] conwayCells;
 
 
     private void Start()
@@ -17,13 +16,32 @@ public class TileManager : MonoBehaviour
             return;
         }
 
-        tiles = new GameObject[WIDTH,HEIGHT];
+        tiles = new SpriteRenderer[WIDTH,HEIGHT];
+        conwayCells = new bool[WIDTH,HEIGHT];
 
         for (int i = 0; i < WIDTH; i++)
         {
             for (int j = 0; j < HEIGHT; j++)
             {
-                tiles[i, j] = Instantiate(tilePrefab, new Vector3(i,j,0), Quaternion.identity);
+                tiles[i, j] = Instantiate(tilePrefab, new Vector3(i,j,0), Quaternion.identity).GetComponent<SpriteRenderer>();
+                conwayCells[i, j] = 0 == Random.Range(0, 2);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        for (int i = 0;i < WIDTH;i++)
+        {
+            for(int j = 0;j < HEIGHT;j++) 
+            {
+                if (conwayCells[i,j])
+                {
+                    tiles[i, j].GetComponent<SpriteRenderer>().color = Color.black;
+                } else
+                {
+                    tiles[i, j].GetComponent<SpriteRenderer>().color = Color.white;
+                }
             }
         }
     }
