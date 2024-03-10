6476171471a7f883de54432a3cc555e7c4fd743f
Aron Fannarsson <aron.orri@gmail.com>
2024-03-10 16:11:13 -0400

    Added scaling to camera movement

--- Assets/Naive/Scripts/CameraMovement.cs
+++ Assets/Naive/Scripts/CameraMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     private Camera mainCamera;
-    [SerializeField] private float zoomScale = 1f;
+    [SerializeField] private float zoomScale = 1f, moveScale = 1f;
 
     private void Start()
     {
@@ -17,7 +17,7 @@ public class CameraMovement : MonoBehaviour
         Vector2 scrollVector = Input.mouseScrollDelta;
         mainCamera.orthographicSize -= scrollVector.y * zoomScale;
 
-        Vector3 moveVector = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        Vector3 moveVector = new Vector3(Input.GetAxis("Horizontal") * moveScale, Input.GetAxis("Vertical") * moveScale, 0);
         transform.position += moveVector;
     }
 }
